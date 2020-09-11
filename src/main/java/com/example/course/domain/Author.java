5d84420e37a43ce4d4651222666f584c9ab915f2
Nikita Gritcaenko <Nikita_Gritcaenko@epam.com>
2020-09-12 00:43:52 +0300

    add authors endpoint

--- src/main/java/com/example/course/domain/Author.java
+++ src/main/java/com/example/course/domain/Author.java
@@ -27,6 +27,10 @@ public class Author {
         this.lastName = lastName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
