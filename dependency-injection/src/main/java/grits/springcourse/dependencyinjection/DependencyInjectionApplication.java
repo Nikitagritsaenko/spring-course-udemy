22fc5242a29415fbb7672e85cc688b82a584ec11
Nikita Gritcaenko <Nikita_Gritcaenko@epam.com>
2020-09-12 23:40:13 +0300

    add profiles

--- dependency-injection/src/main/java/grits/springcourse/dependencyinjection/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/grits/springcourse/dependencyinjection/DependencyInjectionApplication.java
@@ -1,9 +1,7 @@
 package grits.springcourse.dependencyinjection;
 
-import grits.springcourse.dependencyinjection.controllers.ConstructorInjectedController;
-import grits.springcourse.dependencyinjection.controllers.MyController;
-import grits.springcourse.dependencyinjection.controllers.PropertyInjectedController;
-import grits.springcourse.dependencyinjection.controllers.SetterInjectedController;
+import com.sun.org.apache.xml.internal.security.utils.I18n;
+import grits.springcourse.dependencyinjection.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +12,10 @@ public class DependencyInjectionApplication {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+        I18nController i18nController = (I18nController) applicationContext.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
+
         MyController controller = (MyController) applicationContext.getBean("myController");
 
         System.out.println("---------- PRIMARY -----------");
