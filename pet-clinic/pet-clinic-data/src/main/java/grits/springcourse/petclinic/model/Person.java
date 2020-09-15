f5a45a41572bc0964f3237ac0b1d0019a7136107
Nikita Gritcaenko <nikita-gricaenko@ya.ru>
2020-09-16 01:41:12 +0300

    add base entity

--- pet-clinic/pet-clinic-data/src/main/java/grits/springcourse/petclinic/model/Person.java
+++ pet-clinic/pet-clinic-data/src/main/java/grits/springcourse/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package grits.springcourse.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic/pet-clinic-data/src/main/java/grits/springcourse/petclinic/model/PetType.java
+++ pet-clinic/pet-clinic-data/src/main/java/grits/springcourse/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package grits.springcourse.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
